2ca7455c48e7586e4ebc914adb5808aa030582fc
Delphes1980 <delphine.coutouly-laborda@hotmail.com>
2025-04-08 12:53:49 +0200

    add prototype to binary_trees.h, create 14-binary_tree_balance.c

---
+++ 14-binary_tree_balance.c
@@ -0,0 +1,25 @@
+#include "binary_trees.h"
+
+/**
+* binary_tree_balance - measures the balance factor of a binary tree
+* @tree: a pointer to the root node of the tree to measure the balance factor
+*
+* Return: the balance of the tree. If tree is NULL: return 0
+*/
+
+int binary_tree_balance(const binary_tree_t *tree)
+{
+	int left, right;
+	int max = 0;
+
+	if (tree == NULL)
+	{
+		return (-1);
+	}
+
+	left = binary_tree_balance(tree->left);
+	right = binary_tree_balance(tree->right);
+	max = left - right;
+
+	return (max);
+}
