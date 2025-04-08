d41273ac1c568b9b6adf06e74c6349cf09c8da0c
Delphes1980 <delphine.coutouly-laborda@hotmail.com>
2025-04-08 14:25:51 +0200

    add height calcul function

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -1,7 +1,5 @@
 #include "binary_trees.h"
 
-#include "binary_trees.h"
-
 /**
 * binary_tree_height - measures the height of a binary tree
 * @tree: a pointer to the root node of the tree to measure the height.
@@ -52,12 +50,12 @@ int binary_tree_balance(const binary_tree_t *tree)
 
 	if (tree->left != NULL)
 	{
-		left = binary_tree_balance(tree->left) + 1;
+		left = binary_tree_height(tree->left);
 	}
 
 	if (tree->right != NULL)
 	{
-		right = binary_tree_balance(tree->right) + 1;
+		right = binary_tree_height(tree->right);
 	}
 
 	max = left - right;
