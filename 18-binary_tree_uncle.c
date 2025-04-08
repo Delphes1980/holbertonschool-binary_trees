5bc1593e30da8dc67e0e8518aeec6e680854056f
Delphes1980 <delphine.coutouly-laborda@hotmail.com>
2025-04-08 19:47:59 +0200

    fix output error

--- 18-binary_tree_uncle.c
+++ 18-binary_tree_uncle.c
@@ -11,10 +11,20 @@
 
 binary_tree_t *binary_tree_uncle(binary_tree_t *node)
 {
-	if (node == NULL || node->parent == NULL || node->parent->parent == NULL)
+	binary_tree_t *grandparent = node->parent->parent;
+
+	if (node == NULL || node->parent == NULL || grandparent == NULL)
 	{
 		return (NULL);
 	}
 
-	return (binary_tree_uncle(node->parent));
+	if (grandparent->left == node->parent)
+	{
+		return (grandparent->right);
+	}
+
+	else
+	{
+		return (grandparent->left);
+	}
 }
